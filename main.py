a1fb015b9f757aa1434f77e356df864a871ddf50
Oleg Shpagin <info@wiseplat.org>
2022-01-26 13:03:43 +0300

    update data

--- main.py
+++ main.py
@@ -78,13 +78,12 @@ def main():
         normalise=configs['data']['normalise']
     )
 
-    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'],
-                                                   configs['data']['sequence_length'])
+    # predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
     # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
-    # predictions = model.predict_point_by_point(x_test)
+    predictions = model.predict_point_by_point(x_test)
 
-    plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
-    # plot_results(predictions, y_test)
+    # plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+    plot_results(predictions, y_test)
 
 
 if __name__ == '__main__':
